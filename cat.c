85def45ce5138bf195425334d7f8683777b91402
tatsuki kanda <tatsuki1112@gmail.com>
2021-05-07 00:35:52 +0900

    start coding cat command

---
+++ cat.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+static void do_cat(const char *path);
+static void die(const char *s);
